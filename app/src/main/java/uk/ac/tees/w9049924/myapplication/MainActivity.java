c1381e5e46521fcc1470836244c278fd9a919f03
Aaron <Aaronbaldwin2000@hotmail.co.uk>
2022-01-28 14:51:00 +0000

    In the middle of adding a way to send an email - currently no onClick on button, that was the next step

--- app/src/main/java/uk/ac/tees/w9049924/myapplication/MainActivity.java
+++ app/src/main/java/uk/ac/tees/w9049924/myapplication/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import static java.net.Proxy.Type.HTTP;
+
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
@@ -22,5 +24,22 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message); //Adds edit text to the intent - intents can carry data types called extras
         startActivity(intent);
+    }
+
+    public void sendEmail(View view)
+    {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+// The intent does not have a URI, so declare the "text/plain" MIME type
+        EditText emailMessage = (EditText) findViewById(R.id.editTextEmail);
+        EditText emailAddress = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        String email = emailAddress.getText().toString();
+        String emailmessage = emailMessage.getText().toString();
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, email); // recipients
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Email subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, emailmessage);
+        startActivity(emailIntent);
+        //emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("content://path/to/email/attachment"));
+// You can also attach multiple items by passing an ArrayList of Uris
     }
 }
